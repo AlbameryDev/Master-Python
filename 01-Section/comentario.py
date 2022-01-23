86a6c65a5567067c0cd695167cc038465b9a4a40
Alba Maria <albimrs@gmail.com>
2022-01-23 21:09:29 +0100

    coments

---
+++ 01-Section/comentario.py
@@ -0,0 +1,13 @@
+# Enviando hola mundo a consola
+print("Hola mundo, estoy aprendiendo Python")
+# Enviando datos de mi profesion
+print ("La odontología es una de las ramas mas interesantes de la sanidad")
+# Enviando datos que no tienen nada que ver
+print ("Me comí un libro")
+
+"""
+aqui aparece el codigo del coche
+print("programando un coche")
+print ("piezas del coche")
+"""
+print ("primeros comentarios con exito")
