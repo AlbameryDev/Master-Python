a157dd44e2cc98cb4df5f791f6c1ac8b7cb9d2da
Alba Maria <albimrs@gmail.com>
2022-01-28 20:39:51 +0100

    Variables y tipos de datos

---
+++ 02-variables-y-tipos/variables.py
@@ -0,0 +1,25 @@
+"""
+Una variable es un contenedor de información 
+que dentro guardará un dato; se pueden crear
+muchas variables y que cada una tenga un dato distinto.
+"""
+
+variable_texto = "Me gusta la odontología"
+variable_numero = 570
+variable_decimal = 35.7
+
+print(variable_texto)
+print(variable_numero)
+print(variable_decimal)
+
+print("---------------------------------------------------------")
+
+variable_texto = "Me gusta Python"
+variable_numero = 1800
+variable_decimal = 50.5
+
+
+
+print(variable_texto)
+print(variable_numero)
+print(variable_decimal)
