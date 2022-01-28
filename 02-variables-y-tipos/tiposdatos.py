914358c4f6c36af3583f1150a88ad234d82a9d6e
Alba Maria <albimrs@gmail.com>
2022-01-28 21:54:58 +0100

    tiposdedatos

---
+++ 02-variables-y-tipos/tiposdatos.py
@@ -0,0 +1,18 @@
+nada = None
+cadena_string = "Hola soy Alba Reina"
+numero_entero = 25
+numero_flotante = 22.5
+boolean = True 
+lista = [5, 10, 15, 20]
+listaMixta = [5, "cinco", 10.4, "diez"] 
+tuplaNoCambia = ("Master", "en", "Python")
+diccionario = {
+    "nombre": "Alba Maria",
+    "Apellidos": "Reina Sanchez",
+    "Profesion": "Odontologa"
+}
+rango = range(20)
+dato_byte = b"prueba byte"
+
+print(dato_byte)
+print(type(dato_byte))
