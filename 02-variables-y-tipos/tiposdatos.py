581560bfa2081e6e7b1a35dbd81dced1d9091b7e
Alba Maria <albimrs@gmail.com>
2022-01-28 22:10:50 +0100

    convertirTiposdeDatos

--- 02-variables-y-tipos/tiposdatos.py
+++ 02-variables-y-tipos/tiposdatos.py
@@ -1,3 +1,4 @@
+#tipos de datos
 nada = None
 cadena_string = "Hola soy Alba Reina"
 numero_entero = 25
@@ -14,5 +15,21 @@ diccionario = {
 rango = range(20)
 dato_byte = b"prueba byte"
 
+#imprimir variable
 print(dato_byte)
+
+#imprimir el tipo de variable
 print(type(dato_byte))
+
+#convertir datos
+texto = "Hola soy un texto"
+numero = 1557
+numero = str(1557)
+
+print(texto + " " + numero)
+
+numero = float(1557)
+print(type(numero))
+
+numero = int(1557)
+print(type(numero))
