c52c94540c43cf92ff69a0a1f1e0f47d9fe4a9a0
Alba Maria <albimrs@gmail.com>
2022-01-29 22:06:19 +0100

    Operadores Aritmeticos

---
+++ 03-operadores/aritmeticos.py
@@ -0,0 +1,16 @@
+#operadores aritmeticos
+
+numero1 = 570
+numero2 = 1100
+
+resta = numero1 - numero2
+suma = numero1 + numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+resto = numero1 % numero2
+
+print(f"La resta de {numero1} - {numero2} = {resta} ")
+print(f"La suma de {numero1} + {numero2} = {suma} ")
+print(f"La multiplicacion de {numero1} * {numero2} = {multiplicacion} ")
+print(f"La division de {numero1} / {numero2} = {division} ")
+print(f"El resto de {numero1} % {numero2} = {resto} ")
