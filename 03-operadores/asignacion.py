0d16bd41e84069a8db3fc4ce1ca0589f63513238
Alba Maria <albimrs@gmail.com>
2022-01-29 22:19:29 +0100

    Operadores de Asignación

---
+++ 03-operadores/asignacion.py
@@ -0,0 +1,19 @@
+#Operadores de Asignación
+
+edad = 25 
+print(edad)
+
+edad += 25
+print(edad)
+
+edad = edad + 5
+print(edad)
+
+edad -= 5
+print(edad)
+
+edad *= 5
+print(edad)
+
+edad /= 5
+print(edad)
