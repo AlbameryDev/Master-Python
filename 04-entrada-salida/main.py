f65096f460502978c91d4080e73c7bc8eaa60fdd
Alba Maria <albimrs@gmail.com>
2022-01-29 22:53:50 +0100

    Entrada y salida de datos

---
+++ 04-entrada-salida/main.py
@@ -0,0 +1,6 @@
+#Entrada de datos
+nombre = input ("¿Como te llamas?:")
+profesion = input("¿A que te dedicas?:")
+
+#Salida de datos
+print(f"Encantada de conocerte {nombre} tu trabajo como {profesion} es maravilloso")
