556d1c53de593fcb2846969e39312c67969f40d3
Alba Maria <albimrs@gmail.com>
2022-02-06 21:10:57 +0100

    Bucle While EJEMPLO

--- 06-bucles/bucle-while.py
+++ 06-bucles/bucle-while.py
@@ -7,8 +7,8 @@ hasta que deje de cumplirse la condicion
 
 
 While condición
-    bloque de intrucciones
-    actualizacion de contador
+bloque de intrucciones
+actualizacion de contador
 
 """
 ### EJEMPLO 1 ###
@@ -29,7 +29,28 @@ muestrame =str (0)
 while contador <=100:
     print(f"Estoy en el número {contador}")
 
+
+
     muestrame = muestrame + ", " + str(contador)
     contador += 1
 
     print(muestrame)
+
+
+
+    #EJEMPLO 3
+
+print("####### EJEMPLO 3#######")
+numero_usuario = int(input("¿De que número quieres la tabla?: "))
+
+if numero_usuario < 1:
+        numero_usuario = 1
+
+print(f" Tabla del {numero_usuario} ")
+
+contador = 1
+while contador <= 10:
+        print(f"{numero_usuario} x {contador} = {numero_usuario*contador}")
+        contador +=1
+else:
+        print("Tabla finalizada")
