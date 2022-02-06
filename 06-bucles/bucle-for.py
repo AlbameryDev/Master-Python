c9ac788ceda5f635bd3014a94284dc41bf98538b
Alba Maria <albimrs@gmail.com>
2022-02-06 20:08:47 +0100

    Bucle for

---
+++ 06-bucles/bucle-for.py
@@ -0,0 +1,17 @@
+"""
+
+#FOR
+
+for variable in elemento_iterable (lista, rango, etc...)
+                BLOQUE DE INSTRUCCIONES
+
+"""
+
+contador = 0
+resultado = 0
+
+for contador in range(0, 10):
+        print ("Voy por el "+str(contador))
+        
+        resultado = resultado + contador
+print(f"El resultado es: {resultado}")
