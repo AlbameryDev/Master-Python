d44dc1ade6447ed9106f55ac8695114c739ccfe0
Alba Maria <albimrs@gmail.com>
2022-02-06 20:37:30 +0100

    Bucle for EJEMPLOS

--- 06-bucles/bucle-for.py
+++ 06-bucles/bucle-for.py
@@ -5,6 +5,9 @@
 for variable in elemento_iterable (lista, rango, etc...)
                 BLOQUE DE INSTRUCCIONES
 
+BREAK 
+! para salir del bucle
+
 """
 
 contador = 0
@@ -15,3 +18,21 @@ for contador in range(0, 10):
         
         resultado = resultado + contador
 print(f"El resultado es: {resultado}")
+
+
+
+#Ejemplo tablas de multiplicar
+print("\n############# EJEMPLO ##########3")
+
+numero_usuario = int(input("¿De que número quieres la tabla?:"))
+valor = range(1,11)
+
+if numero_usuario <1:
+        numero_usuario = 1
+print(f"#### Tabla de multiplicar del número {numero_usuario}")
+
+for numero_tabla in valor:
+
+        print(f"{numero_usuario} x {numero_tabla} = {numero_usuario*numero_tabla}")
+else:
+        print("Tabla finalizada.")
